14bb84cb1fd99fc9265b52b8f936ca90dcde97fa
Pabliim <77503596+Pabliim@users.noreply.github.com>
2021-01-19 17:55:24 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,122 +1,78 @@
 const help = (prefix) => {
 	return `> *BOT HYPE COMANDOS* 😳 <
 
-> ⚙️ INFO BOT ⚙️ <
-⚠️ NAO LIGUE ! SEM SPAM!!! ⚠️
-SOMENTE USAR NO GRUPO OU BLOCK
-grupσ dσ вσt : https://chat.whatsapp.com/JlP3KtMS9esITV937kEJn9
-Dono do bot : https://api.whatsapp.com/send?phone=+5563991084396
-> 🎨 Figurinhas 🎨<
-
-comando : *.sticker* or *.stiker*
-desc : converter imagem / gif / vídeo em adesivo
-uso : responder imagem / gif / vídeo ou enviar imagem / gif / vídeo com legenda
-
-comando: *.dono* or *.info*
-desc : informações do dono e info do bot
-uso : apenas envie o comando
-
-comando : *.toimg*
-desc : converter adesivo em imagem
-uso : reply sticker
-
-comando : *.tsticker* or *.tstiker*
-desc : converter texto em adesivo
-uso : *.tsticker text in here*
-
-
-> ⚙️ 𝗠𝗲𝗺𝗲 𝗖𝗼𝗺𝗮𝗻𝗱𝗼𝘀 ⚙️<
-
-comando : *.meme*
-desc : imagens aleatórias de meme [Portuguese]
-uso : apenas envie o comando
-
-comando : *.memeindo*
-desc : imagens aleatórias de meme [indo]
-uso : apenas envie o comando
-
-
-ɪɴsᴛᴀɢʀᴀᴍ : https://www.instagram.com/pabliim.ip?r=nametag
-
-> ✨𝗢𝘂𝘁®𝗼𝘀 𝗖𝗼𝗺𝗮𝗻𝗱𝗼𝘀✨<
-
-comando : *.gtts*
-desc : converter texto em fala/audio
-usao : *.gtts [cc] [text]*
-example : *.gtts ja On2-chan*
-
-comando : *.loli*
-desc : imagens aleatórias de loli
-uso : apenas envie o comando
-
-comando : *.nsfwloli*
-desc :imagens aleatórias de nsfw loli
-uso : apenas envie o comando
-
-comando : *.url2img*
-desc :tirar screenshots da web
-uso : *.url2img [tipe] [url]*
-
-comando : *.simi*
-desc : esse comando buga simi
-uso : *.simi yourmessage*
-
-comando : *.ocr*
-desc : pegue o texto na foto
-uso : responder imagem ou enviar imagem com legenda
-
-comando : *.wait*
-desc : procure anime com imagem [ What Anime Is This/That ]
-uso : responder imagem ou enviar imagem com legenda
-
-comando : *.setprefix*
-desc : replace prefix
-uso : *.setprefix [text|optional]*
-example : *.setprefix ?*
-nota : Este comando só pode ser usado pelo proprietário do bot
-
-
-> 🔰𝗚𝗿𝘂𝗽𝗼 𝗖𝗼𝗺𝗮𝗻𝗱𝗼𝘀🔰 <
-
-Numero pablo : http://wa.me/+5563991084396
-
-comando : *.kick*
-desc : remove membros do grupo
-usar : *.kick @tagmember*
-
-nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin
-
-comando : *.promote*
-desc : tornar o membro do grupo como administrador do grupo
-usar : *.promote @tagmember*
-
-nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!
-
-comando : *.demote*
-desc : tornar o administrador do grupo como membro do grupor
-usar : *.demote @tagmember*
-
-nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!
-
-comando : *.linkgroup*
-desc : pegue o link do grupo
-usar : apenas envie o comando
-nota : só pode ser usado quando o bot se torna admin, e quem envia o comando é admin!
-
-comando : *.leave*
-desc : bot sai do grupo
-uso : apenas envie o comando
-nota : Só pode ser usado por administradores de grupo e proprietário do bot!
-
-comando : *.tagall*
-desc : marca todos os membros do grupo, incluindo administradores também
-usage : apenas envie o comando 
-nota : Este comando pode ser usado se você for um administrador de grupo
-
-command : *.simih*
-desc : ative o modo simi no grupo isso buga
-usage : *.simih 1* to activate simi mode and *.simih 0* to deactivate simi mode
-note : comando Simi inútil so serve pra buga o Grupo`
+⚙️ Bot Hype Menu ⚙️
+┏━━━━━━━━━━━━━━━━━━━━┓
+┃ *⚠️ NAO LIGUE ! SEM SPAM!!! ⚠️*
+┃ *SOMENTE USAR NO GRUPO OU BLOCK*
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃⊱❥ DONO: PABLO DO HYPE
+┃⊱❥ PREFIXO : 「 . 」
+┃⊱❥ VERSION : 1.0
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ DADOS DO BOT
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ Grupo whatsapp : https://chat.whatsapp.com/BYrlFly24u26ERQJPKoYyG
+┃➢ Whatsapp 55 63 991084396
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ MENU SIMPLES
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ .sticker - envie o gif ou imagem com a legenda 
+┃➢ .toimg - fazer a figurinha virar foto 
+┃➢ .ttp
+┃➢ .gtts - converter texto em audio 
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ MENU DOWNLOAD
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ .tiktod
+┃➢ .ytmp3
+┣━━━━━━━━━━━━━━━━━━━━┛
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ MENU MEME
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ .meme
+┃➢ .memeindo
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ MENU GRUPO
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ .admin
+┃➢ .welcome [1/0]
+┃➢ .add -adicionar um corno
+┃➢ .kick - remover um ze ruela 
+┃➢ .promote
+┃➢ .demote
+┃➢ .tagall - marcar os gados 
+┃➢ .tagall2
+┃➢ .tagall3
+┃➢ .linkgrup - link dessa porra 
+┃➢ .leave - comando pro bot sair 
+┃➢ .simih [1/0]
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ MENU LOLI [ EMROR ]
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ #loli
+┃➢ #nsfwloli
+┣━━━━━━━━━━━━━━━━━━━━┛
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ OUTRAS PARADA AI
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ .quotes
+┃➢ .ssweb
+┃➢ .simi
+┃➢ .ocr
+┃➢ .wait
+┃➢ .tiktokstalk
+┃➢ .hilih
+┃➢ .ytstalk
+┣━━━━━━━━━━━━━━━━━━━━┛
+┃ MENU DO DONO
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃➢ .setprefix
+┃➢ .bc
+┃➢ .clearall
+┃➢ .clone Muda foto do bot apenas por comando.
+┣━━━━━━━━━━━━━━━━━━━━┛
+┣━━━━━━━━━━━━━━━━━━━━┓`
 }
 
 exports.help = help
